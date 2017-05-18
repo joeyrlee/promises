7a4f6998d0031e1946e1e54de156a9f3855e286c
Joey Lee <jleepianist@gmail.com>
2017-05-17 21:18:53 -0700

    Solution Code

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -1,44 +1,98 @@
 /**
- * Implement these promise-returning functions.
- * Any successful value should be made available in the next `then` block chained
- * to the function invocation, while errors should be available in the `catch` block
- */
+ * Create the promise returning `Async` suffixed versions of the functions below,
+ * Promisify them if you can, otherwise roll your own promise returning function
+ */ 
 
 var fs = require('fs');
 var request = require('request');
+var crypto = require('crypto');
 var Promise = require('bluebird');
 
-// This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+// (1) Asyncronous HTTP request
+var getGitHubProfile = function(user, callback) {
+  var options = {
+    url: 'https://api.github.com/users/' + user,
+    headers: { 'User-Agent': 'request' },
+    json: true  // will JSON.parse(body) for us
+  };
 
+  request.get(options, function(err, res, body) {
+    if (err) {
+      callback(err, null);
+    } else if (body.message) {
+      callback(new Error('Failed to get GitHub profile: ' + body.message), null);
+    } else {
+      callback(null, body);
+    }
+  });
+};
+
+
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
+
+
+// (2) Asyncronous token generation
+var generateRandomToken = function(callback) {
+  crypto.randomBytes(20, function(err, buffer) {
+    if (err) { return callback(err, null); }
+    callback(null, buffer.toString('hex'));
+  });
+};
+
+
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
+
+
+// (3) Asyncronous file manipulation
+var readFileAndMakeItFunny = function(filePath, callback) {
+  fs.readFile(filePath, 'utf8', function(err, file) {
+    if (err) { return callback(err); }
+   
+    var funnyFile = file.split('\n')
+      .map(function(line) {
+        return line + ' lol';
+      })
+      .join('\n');
+
+    callback(funnyFile);
+  });
+};
+
+
+// This function violates rule (2) of the node style callback pattern,
+// therefore we have to reimplement it using the `new Promise` constructor
+var readFileAndMakeItFunnyAsync = function(filePath) {
   return new Promise(function(resolve, reject) {
     fs.readFile(filePath, 'utf8', function(err, file) {
-      if (err) {
-        reject(err);
+      if (err) { return reject(err); }
+      
+      var funnyFile = file.split('\n')
+        .map(function(line) {
+          return line + ' lol';
+        });
+
+      resolve(funnyFile);
+    });
+  });
+};
+
+// Alternatively, we could use our previously written function
+// and do some error checking on the only argument passed to the callback
+readFileAndMakeItFunnyAsync = function(filePath) {
+  return new Promise(function(resolve, reject) {
+    readFileAndMakeItFunny(filePath, function(errorOrFile) {
+      if (errorOrFile instanceof Error) {
+        reject(errorOrFile);
       } else {
-        var firstLine = file.split('\n')[0];
-        resolve(firstLine);
+        resolve(errorOrFile);
       }
     });
   });
-  };
-
-// This function should retrieve the status code of a GET request to `url`
-var getStatusCodeAsync = function(url) {
-  // TODO
-
-  return new Promise(function(resolve, reject) {
-    request.get(url, function(err, response, body) {
-      // An alternate way to implement our error guard
-      if (err) { return reject(err); }
-      resolve(response.statusCode);
-    });
-  });
-  };
+};
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
-  getStatusCodeAsync: getStatusCodeAsync,
-  pluckFirstLineFromFileAsync: pluckFirstLineFromFileAsync
+  getGitHubProfileAsync: getGitHubProfileAsync,
+  generateRandomTokenAsync: generateRandomTokenAsync,
+  readFileAndMakeItFunnyAsync: readFileAndMakeItFunnyAsync
 };
